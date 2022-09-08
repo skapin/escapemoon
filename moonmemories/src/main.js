ae6a5b61b0b53a23f881bca64cb83b538767fedb
SkapinTheFourb's <skapinthefourb@gmail.com>
2022-09-09 00:34:09 +0200

    [INITIAL]

---
+++ moonmemories/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+import OpenLayersMap from 'vue3-openlayers'
+import 'vue3-openlayers/dist/vue3-openlayers.css'
+
+
+import "./assets/main.css";
+
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(OpenLayersMap)
+
+app.mount("#app");
